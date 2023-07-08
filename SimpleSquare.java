c55c17612037da5d0f06d9f59d2a74ff1a499d27
Akash1305akki <88767034+Akash1305akki@users.noreply.github.com>
2023-07-08 20:03:02 +0530

    Add files via upload

---
+++ SimpleSquare.java
@@ -0,0 +1,27 @@
+package PatternPrograms;
+
+import java.util.Scanner;
+
+public class SimpleSquare {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] Ak) throws Exception{
+
+        System.out.print("Enter the value of N(No.of lines) to do printing: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if (true){
+                    System.out.print(" * ");
+                }else {
+                    System.out.print("   ");
+                }
+            }
+            Thread.sleep(500);
+            System.out.println();
+        }
+
+    }
+}
---
+++ SquareWithDiagonals.java
@@ -0,0 +1,28 @@
+package PatternPrograms;
+
+import java.util.Scanner;
+
+public class SquareWithDiagonals {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] Ak) throws Exception{
+
+        System.out.print("Enter the value of N(No.of lines) to do printing: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if ((i==0||i==n-1)||(j==0||j==n-1)||(i==j||i+j==n-1)){
+                    System.out.print(" * ");
+                }else {
+                    System.out.print("   ");
+                }
+            }
+            Thread.sleep(500);
+            System.out.println();
+        }
+
+    }
+}
